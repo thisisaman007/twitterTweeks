c99daf06b2196089a55ff5c25affb9a6e6b8e6cf
thisisaman007 <thisisaman007@gmail.com>
2020-06-28 13:26:56 +0530

    os errors

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -4,10 +4,10 @@ import os
 
 FILE_NAME ='last_seen_id.txt'
 
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
